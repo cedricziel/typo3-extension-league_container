45cdbb62302b5fe79dafe653317705db16215f6d
Cedric Ziel <cedric@cedric-ziel.com>
2015-06-02 21:18:01 +0200

    [TASK] Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'The PHP league - container',
+	'description' => '',
+	'category' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'clearCacheOnLoad' => 1,
+	'version' => '1.3.2',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.3.99',
+		),
+		'conflicts' => array(),
+		'suggests' => array(),
+	),
+);
